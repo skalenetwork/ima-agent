f4fd8d604a6a1221d6c8e4731613f29e73f1aab8
Sergiy Lavrynenko <sergiy@skalelabs.com>
2023-11-15 17:48:30 +0000

    forgotten condition added

--- src/bls.mjs
+++ src/bls.mjs
@@ -1206,7 +1206,9 @@ async function doSignProcessHandleCall(
         "skale_imaVerifyAndSign", i, strNodeURL, optsSignOperation.fromChainName,
         optsSignOperation.targetChainName, joParams, joOut, optsSignOperation.sequenceId );
     if( ( !joOut ) || typeof joOut != "object" || ( !( "result" in joOut ) ) || ( !joOut.result ) ||
-        typeof joOut.result != "object" ) {
+            typeof joOut.result != "object" ||
+            ( !( "signature" in joOut.result ) ) || joOut.result.signature != "object"
+    ) {
         ++optsSignOperation.joGatheringTracker.nCountErrors;
         if( log.id != optsSignOperation.details.id ) {
             log.critical( "{p}S-Chain node {} reported wallet error: {err}, sequence ID is ",
