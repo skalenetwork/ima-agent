3500e5220d32d653bd507b94c516f352e6d54ddd
Dmytro <dmitry.tkk@gmail.com>
2023-11-29 13:10:32 +0000

    ima-agent#10 fix ethers hotfix script

--- network-browser/ethers_hotfix.sh
+++ network-browser/ethers_hotfix.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Temporary hotfix for ethers/bun.sh issue
 # Fix be removed when https://github.com/oven-sh/bun/issues/5309 is fixed
-FILE="node_modules/ethers/lib.esm/utils/geturl.js"
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FILE="$DIR/node_modules/ethers/lib.esm/utils/geturl.js"
+
 if [ -f "$FILE" ]; then
     sed -i '' 's/body = getBytes(gunzipSync(body));/body = getBytes(gunzipSync(Buffer.from(body)));/' "$FILE"
 
